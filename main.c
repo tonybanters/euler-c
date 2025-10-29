e6c37eb9e6fad8890fde12a6417cc665ca8b0579
tonybtw <tonybtw@tonybtw.com>
2025-10-28 23:26:49 -0700

    added problem3

--- main.c
+++ main.c
@@ -13,5 +13,7 @@ int main(void) {
     printf("Problem 1: %d\n", sum_of_multiples(1000));
     printf("Problem 2 (Optimized): %ld\n", sum_of_even_fibs_optimized(4000000));
     printf("Testing is_prime: 31 - %s\n", is_prime(31) ? "Prime" : "Composite");
+    printf("Testing is_prime: 13195 - %s\n", is_prime(13195) ? "Prime" : "Composite");
+    printf("Problem 3: %ld\n", largest_prime_factor(600851475143));
     return 0;
 }
--- problem3.h
+++ problem3.h
@@ -18,5 +18,28 @@ bool is_prime(int n) {
     return true;
 }
 
+long largest_prime_factor(long n) {
+    if(n == 1){
+        // handle case where 1 is given in.. 1 has no prime factors otherwise
+        // the fundamental theorem of arithmetic would fail
+        return 0;
+    }
+
+    long largest = (n % 2 == 0) ? 2 : 1;
+    long i = 3;
+    while (i * i <= n) {
+        if (n % i == 0) {
+            if(is_prime(n / i)){
+                largest = n / i;
+            }
+            else if(is_prime(i)){
+                largest = i;
+            }
+        }
+        i += 2;
+    }
+    return largest != 1 ? largest : n;
+}
+
 #endif
 
